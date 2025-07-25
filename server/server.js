f40ebb524ca421c2a7e10a29175b4b155678254e
PriyanshiNmeher <167795594+PriyanshiNmeher@users.noreply.github.com>
2025-07-25 11:46:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,6 +40,6 @@ app.use('/api/order', orderRouter)
 
 
 app.listen(port, ()=>{
-    console.log(`Server is running on http://localhost:${port}`)
+    console.log(`Server is running on http://localhost:${port}`, `https://streetway-backend.onrender.com/`)
 })
 
